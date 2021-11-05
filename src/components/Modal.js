67793c612df836c97cbe069fc283859d32785411
Simply Elvis <provictor.ie@gmail.com>
2021-11-05 11:25:43 +0100

    model display component.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-export const Modal = ({ selectedImg }) => {
+export const Modal = ({ selectedImg, setSelectedImg }) => {
+  const handleBackdrop = (e) => {
+    if (e.target.classList.contains("backdrop")) {
+      setSelectedImg(null);
+    }
+  };
+
   return (
-    <div className="backdrop">
+    <div className="backdrop" onClick={handleBackdrop}>
       <img src={selectedImg} alt=" enlarged display " />
     </div>
   );
