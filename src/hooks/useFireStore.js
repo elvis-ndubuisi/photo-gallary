d434c97709301d9f0e77b518c2dae03bee0394e8
Simply Elvis <provictor.ie@gmail.com>
2021-11-05 10:38:38 +0100

    added orderBy.

--- src/hooks/useFireStore.js
+++ src/hooks/useFireStore.js
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
 import { database } from "../firebase/config";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, orderBy } from "firebase/firestore";
 
 const useFireStore = (coll) => {
   const [docs, setDocs] = useState([]);
 
   useEffect(
     function () {
-      const unsubscribe = onSnapshot(collection(database, coll), (snapShot) => {
-        const documents = [];
-        snapShot.forEach((doc) => {
-          documents.push({ ...doc.data(), id: doc.id });
-        });
-        setDocs(documents);
-      });
+      const unsubscribe = onSnapshot(
+        collection(database, coll),
+        orderBy("createdAt", "desc"),
+        (snapShot) => {
+          const documents = [];
+          snapShot.forEach((doc) => {
+            documents.push({ ...doc.data(), id: doc.id });
+          });
+          setDocs(documents);
+        }
+      );
       return () => unsubscribe();
     },
     [coll]
